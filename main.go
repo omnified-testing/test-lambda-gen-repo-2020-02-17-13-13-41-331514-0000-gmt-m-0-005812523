86b341be43469e9cff7cefb31c4816d4f88a0143
Adam Harris <adam@omnified.io>
2020-02-17 13:13:46 +0000

    Auto Generated boilerplate file.

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	utility "github.com/omnified-testing/test-lambda-gen-repo-2020-02-17-13-13-41-331514-0000-gmt-m-0-005812523/utility"
+)
+
+func main() {
+}
+
+// Call : The executable API of the function, just calls the subpackage.
+// Input and output needs to match that of utility subpackage's Call function
+func Call() {
+	utility.Call()
+}
